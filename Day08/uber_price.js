6cc790ea83c7ca49e2d598ca3c420df991d25dc5
mickey61295 <31827390+mickey61295@users.noreply.github.com>
2021-12-08 13:10:34 +0530

    Update uber_price.js

--- Day08/uber_price.js
+++ Day08/uber_price.js
@@ -4,9 +4,10 @@ class UberPrice {
         this.time = time;
     }
     calculatePrice() {
-        console.log(this.distance * 0.25 + this.time * 0.5);
+        console.log(`Price for riding ${this.distance} for ${this.time} is `,this.distance * 0.25 + this.time * 0.5);
     }
 }
 
 
 ride1 = new UberPrice(2, 30);
+ride1.calculatePrice();
