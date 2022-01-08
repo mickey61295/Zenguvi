538bed273f15d4a091bb07fac83af6f9b57a0221
mickey61295 <31827390+mickey61295@users.noreply.github.com>
2022-01-09 01:51:09 +0530

    Add files via upload

---
+++ Hackathon_08_01/script.js
@@ -0,0 +1,45 @@
+let entries;
+let err;
+
+const getEntries = async () => {
+  try {
+    entries = await (
+      await fetch("https://api.openbrewerydb.org/breweries")
+    ).json();
+  } catch (e) {
+    err = e && e.message;
+  }
+};
+
+const boxSelector = document.querySelector(".box");
+const loadingSel = document.querySelector(".loading");
+getEntries()
+  .then(() => {
+    loadingSel.style.display = "none";
+    entries.forEach((entry) => {
+      const box = document.createElement("div");
+      const { name, street, brewery_type, city, state, website_url, phone} = entry;
+      box.classList.add("box-container");
+      box.innerHTML = `
+    <div class="box-header">
+        <h2>${name}</h1>
+        <h3>Type: ${brewery_type}</h3>
+        <h3>Street: ${street}</h3>
+        <h3>Place: ${city}, ${state}</h3>
+      </div>
+    <div class="box-header">
+    <div class="btn-icons">
+    <a href=${website_url}><img src="https://cdn-icons-png.flaticon.com/512/25/25284.png" alt=${website_url} id="imgcontainer"></img></a>
+    <a href="tel:${phone}"><img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/phone-512.png" alt=${phone} id="imgcontainer"></img></a>
+    </div>
+    `;
+      boxSelector.appendChild(box);
+    });
+  })
+  .catch((e) => {
+    loadingSel.style.display = "none";
+    const box = document.createElement("div");
+    box.classList.add("error");
+    box.innerHTML = `<div>Something went wrong: ${e.message}. Please try later </div>`;
+    boxSelector.appendChild(box);
+  });
