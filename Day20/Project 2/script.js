f14da807f7d6b4e1a45d09fe9b9d4db029f5d8bb
Maheshbabu Jeyaraj <j.maihby@gmail.com>
2022-04-15 13:13:37 +0530

    Anime site

--- Day20/Project 2/script.js
+++ Day20/Project 2/script.js
@@ -1,60 +1,64 @@
-function boilerplate(){
-    const body = document.body;
+function boilerplate() {
+	const body = document.body
 
-    const outerDiv = document.createElement('div');
-    outerDiv.classList.add('container-fluid');
-    body.appendChild(outerDiv);
+	const outerDiv = document.createElement('div')
+	outerDiv.classList.add('container-fluid')
+	body.appendChild(outerDiv)
 
-    const boxContainer = document.createElement('div');
-    boxContainer.classList.add('boxContainer');
-    body.appendChild(boxContainer);
+	const boxContainer = document.createElement('div')
+	boxContainer.classList.add('boxContainer')
+	body.appendChild(boxContainer)
 
-    const title = document.createElement('h1');
-    title.classList.add('text-center');
-    title.innerHTML = 'Free to Play Games Database';
-    outerDiv.appendChild(title);
+	const title = document.createElement('h1')
+	title.classList.add('text-center')
+	title.innerHTML = 'Free to Play Games Database'
+	outerDiv.appendChild(title)
 
-    const description = document.createElement('p');
-    description.classList.add('text-center');
-    description.innerHTML = 'This is a list of free to play games from the Giant Bomb API';
-    outerDiv.appendChild(description);
+	const description = document.createElement('p')
+	description.classList.add('text-center')
+	description.innerHTML =
+		'This is a list of free to play games from the Giant Bomb API'
+	outerDiv.appendChild(description)
 }
 
-async function getData(){
-    await boilerplate();
-    var url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
-    // add try catch with fetch and error message
-    var data = await fetch(url, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
-		"x-rapidapi-key": "a5f5fe63e8msh7e37b0466d79e9cp1576a3jsn8568c7d5e78d"
-	}
-    });
-    var parsedData = await data.json();
-    createBoxes(parsedData);
+async function getData() {
+	await boilerplate()
+	var url = 'https://api.aniapi.com/v1/anime'
+	// add try catch with fetch and error message
+	var data = await fetch(url, {
+		method: 'GET',
+		headers: {
+			Authorization:
+				'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyNDUiLCJuYmYiOjE2NDU2OTk0ODEsImV4cCI6MTY0ODI5MTQ4MSwiaWF0IjoxNjQ1Njk5NDgxfQ.is3Wiu4OdGCddM_f7Qcj1-QVYgoHayTvGJRR_8yKGQs',
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+		},
+	})
+	var parsedData = await data.json()
+	createBoxes(parsedData.data.documents)
 }
 
-function createBoxes(data){
-    let boxContainer = document.querySelector('.boxContainer');
-    for (i of data){
-        let box = document.createElement('div');
-        box.classList.add('box');
-        box.setAttribute('style', 'display: none');
-        box.classList.add('col-lg-4');
-        box.classList.add('col-md-6');
-        box.classList.add('col-sm-6');
-        box.classList.add('col-xs-12'); 
-        box.innerHTML = `
-        <img class="thumbnail" src="${i.thumbnail}" alt="${i.title}">
+function createBoxes(data) {
+	let boxContainer = document.querySelector('.boxContainer')
+	for (i of data) {
+		let box = document.createElement('div')
+		box.classList.add('box')
+		box.setAttribute('style', 'display: none')
+		box.classList.add('col-lg-4')
+		box.classList.add('col-md-6')
+		box.classList.add('col-sm-6')
+		box.classList.add('col-xs-12')
+		box.innerHTML = `
+        <img class="thumbnail" src="${i.cover_image}" alt="${i.titles.en}">
         <div class="text-container">
-        <a href="${i.game_url}" target="_blank">${i.title}</a>
-        <p>${i.short_description}</p>
-        <p>Platforms: ${i.platform}</p>
+        <a href="${i.trailer_url}" target="_blank">${i.titles.en}</a>
+        <p>${i.descriptions.en}</p>
+        <p>Episodes: ${i.episodes_count}</p>
+        <p>End Date: ${i.end_date}</p>
         </div>`
-        boxContainer.appendChild(box);
-    }
-    document.body.innerHTML += `<div id="buttons" class="d-flex justify-content-center">
+		boxContainer.appendChild(box)
+	}
+	document.body.innerHTML += `<div id="buttons" class="d-flex justify-content-center">
     <button class="btn btn-light" id="first">First</button>
     <button class="btn btn-light" id="prev">Previous</button>
     <div id="custom-buttons"></div>
@@ -66,99 +70,96 @@ function createBoxes(data){
         <option value="50">50</option>
     </select>
 </div>`
-pagination();
+	pagination()
 }
 
-
-function pagination(){
-    let data = document.querySelectorAll('.box');
-    let buttons = document.querySelector('#custom-buttons');
-    let pageSelect = document.querySelector('#page-select');
-    let first = document.querySelector('#first');
-    let prev = document.querySelector('#prev');
-    let next = document.querySelector('#next');
-    let last = document.querySelector('#last');
-    let page = 1;
-    let pageSize = 20;
-    let totalPages = Math.ceil(data.length / pageSize);
-    let pageButtons = [];
-    for (let i = 1; i <= totalPages; i++){
-        pageButtons.push(i);
-    }
-    pageButtons.forEach(function(button){
-        let buttonElement = document.createElement('button');
-        buttonElement.classList.add('btn');
-        buttonElement.classList.add('btn-light');
-        buttonElement.innerHTML = button;
-        buttons.appendChild(buttonElement);
-    });
-    let pageButtonsElements = document.querySelectorAll('#custom-buttons button');
-    pageButtonsElements.forEach(function(button){
-        button.addEventListener('click', function(){
-            page = button.innerHTML;
-            showPage(page);
-        });
-    });
-    pageSelect.addEventListener('change', function(){
-        pageSize = parseInt(this.value);
-        totalPages = Math.ceil(data.length / pageSize);
-        pageButtons = [];
-        for (let i = 1; i <= totalPages; i++){
-            pageButtons.push(i);
-        }
-        buttons.innerHTML = '';
-        pageButtons.forEach(function(button){
-            let buttonElement = document.createElement('button');
-            buttonElement.classList.add('btn');
-            buttonElement.classList.add('btn-light');
-            buttonElement.innerHTML = button;
-            buttons.appendChild(buttonElement);
-        });
-        pageButtonsElements = document.querySelectorAll('#custom-buttons button');
-        pageButtonsElements.forEach(function(button){
-            button.addEventListener('click', function(){
-                page = button.innerHTML;
-                showPage(page,pageSize);
-            });
-        });
-        showPage(page,pageSize);
-    });
-    first.addEventListener('click', function(){
-        page = 1;
-        showPage(page,pageSize);
-    });
-    prev.addEventListener('click', function(){
-        if (page > 1){
-            page--;
-            showPage(page,pageSize);
-        }
-    });
-    next.addEventListener('click', function(){
-        if (page < totalPages){
-            page++;
-            showPage(page,pageSize);
-        }
-    });
-    last.addEventListener('click', function(){
-        page = totalPages;
-        showPage(page,pageSize);
-    });
-    showPage(page,pageSize);
+function pagination() {
+	let data = document.querySelectorAll('.box')
+	let buttons = document.querySelector('#custom-buttons')
+	let pageSelect = document.querySelector('#page-select')
+	let first = document.querySelector('#first')
+	let prev = document.querySelector('#prev')
+	let next = document.querySelector('#next')
+	let last = document.querySelector('#last')
+	let page = 1
+	let pageSize = 20
+	let totalPages = Math.ceil(data.length / pageSize)
+	let pageButtons = []
+	for (let i = 1; i <= totalPages; i++) {
+		pageButtons.push(i)
+	}
+	pageButtons.forEach(function (button) {
+		let buttonElement = document.createElement('button')
+		buttonElement.classList.add('btn')
+		buttonElement.classList.add('btn-light')
+		buttonElement.innerHTML = button
+		buttons.appendChild(buttonElement)
+	})
+	let pageButtonsElements = document.querySelectorAll('#custom-buttons button')
+	pageButtonsElements.forEach(function (button) {
+		button.addEventListener('click', function () {
+			page = button.innerHTML
+			showPage(page)
+		})
+	})
+	pageSelect.addEventListener('change', function () {
+		pageSize = parseInt(this.value)
+		totalPages = Math.ceil(data.length / pageSize)
+		pageButtons = []
+		for (let i = 1; i <= totalPages; i++) {
+			pageButtons.push(i)
+		}
+		buttons.innerHTML = ''
+		pageButtons.forEach(function (button) {
+			let buttonElement = document.createElement('button')
+			buttonElement.classList.add('btn')
+			buttonElement.classList.add('btn-light')
+			buttonElement.innerHTML = button
+			buttons.appendChild(buttonElement)
+		})
+		pageButtonsElements = document.querySelectorAll('#custom-buttons button')
+		pageButtonsElements.forEach(function (button) {
+			button.addEventListener('click', function () {
+				page = button.innerHTML
+				showPage(page, pageSize)
+			})
+		})
+		showPage(page, pageSize)
+	})
+	first.addEventListener('click', function () {
+		page = 1
+		showPage(page, pageSize)
+	})
+	prev.addEventListener('click', function () {
+		if (page > 1) {
+			page--
+			showPage(page, pageSize)
+		}
+	})
+	next.addEventListener('click', function () {
+		if (page < totalPages) {
+			page++
+			showPage(page, pageSize)
+		}
+	})
+	last.addEventListener('click', function () {
+		page = totalPages
+		showPage(page, pageSize)
+	})
+	showPage(page, pageSize)
 }
 
-function showPage(page,pageSize=20){
-    let data = document.querySelectorAll('.box');
-    let startIndex = (page - 1) * pageSize;
-    let endIndex = startIndex + pageSize;
-    for (let i = 0; i < data.length; i++){
-        if (i >= startIndex && i < endIndex){
-            data[i].style.display = 'flex';
-        } else {
-            data[i].style.display = 'none';
-        }
-    }
+function showPage(page, pageSize = 20) {
+	let data = document.querySelectorAll('.box')
+	let startIndex = (page - 1) * pageSize
+	let endIndex = startIndex + pageSize
+	for (let i = 0; i < data.length; i++) {
+		if (i >= startIndex && i < endIndex) {
+			data[i].style.display = 'flex'
+		} else {
+			data[i].style.display = 'none'
+		}
+	}
 }
 
-
-
-getData();
+getData()
