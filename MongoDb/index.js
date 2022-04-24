85659a025a55a4c456faf8e3a254120ff423a030
Maheshbabu Jeyaraj <j.maihby@gmail.com>
2022-04-24 18:38:42 +0530

    Node and day31 task

--- MongoDb/index.js
+++ MongoDb/index.js
@@ -1,103 +1,83 @@
 import express from 'express'
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
 const app = express()
+
+dotenv.config()
 
 const PORT = 4000
 
-const movies = [
-	{
-		id: '100',
-		name: 'Iron man 2',
-		poster:
-			'https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg',
-		rating: 7,
-		summary:
-			'With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.',
-		trailer: 'https://www.youtube.com/embed/wKtcmiifycU',
-	},
-	{
-		id: '101',
-		name: 'No Country for Old Men',
-		poster:
-			'https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg',
-		rating: 8.1,
-		summary:
-			"A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
-		trailer: 'https://www.youtube.com/embed/38A__WT3-o0',
-	},
-	{
-		id: '102',
-		name: 'Jai Bhim',
-		poster:
-			'https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg',
-		summary:
-			'A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case',
-		rating: 8.8,
-		trailer: 'https://www.youtube.com/embed/nnXpbTFrqXA',
-	},
-	{
-		id: '103',
-		name: 'The Avengers',
-		rating: 8,
-		summary:
-			"Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-		poster:
-			'https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg',
-		trailer: 'https://www.youtube.com/embed/eOrNdBpGMv8',
-	},
-	{
-		id: '104',
-		name: 'Interstellar',
-		poster: 'https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg',
-		rating: 8.6,
-		summary:
-			'When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.',
-		trailer: 'https://www.youtube.com/embed/zSWdZVtXT7E',
-	},
-	{
-		id: '105',
-		name: 'Baahubali',
-		poster: 'https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg',
-		rating: 8,
-		summary:
-			'In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.',
-		trailer: 'https://www.youtube.com/embed/sOEg_YZQsTI',
-	},
-	{
-		id: '106',
-		name: 'Ratatouille',
-		poster:
-			'https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=',
-		rating: 8,
-		summary:
-			'Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.',
-		trailer: 'https://www.youtube.com/embed/NgsQ8mVkN8w',
-	},
-]
-var ids = []
-for (i of movies) {
-	ids.push(i.id)
+const MONGO_URL = process.env.MONGO_URL
+
+// const MONGO_URL = 'mongodb://localhost'
+
+// Connect to MongoDB
+
+async function createConnection() {
+	const client = new MongoClient(MONGO_URL)
+	await client.connect()
+	console.log('Connected to MongoDB')
+	return client
 }
 
+
+const client = await createConnection()
+
+// Add express middleware to parse JSON bodies automatically
+
+app.use(express.json())
+
+// Read collection from MongoDB
+const movies = client.db('classMongo').collections.movies
+
+// Basic request handler
 app.get('/', function (req, res) {
 	res.send('Hello World')
 })
 
 app.get('/movies', function (req, res) {
-	res.send(movies)
+	const { rating } = req.query
+
+	rating
+		? res.send(
+				movies.filter((movie) => {
+					return movie.rating == rating
+				})
+		  )
+		: res.send(movies)
 })
 
-app.get('/movies/:id', function (req, res) {
-	const { id } = req.params
+app.post('/movies', async function (req, res) {
+	const newMovies = req.body
+	console.log(newMovies)
+	const result = await client
+		.db('classMongo')
+		.collection('movies')
+		.insertMany(newMovies)
+	res.send(result)
+})
 
-	movies.find((movie) => {
-		return movie.id === id
+app.get('/movies/:id', async function (req, res) {
+	const { id } = req.params
+	const movie = await client.db('classMongo').collection('movies').findOne({
+		id: id,
 	})
+	movie
 		? res.send(movie)
 		: res
 				.status(404)
 				.send(
 					'<h1 style="text-align:center">Error 404<br/><hr/>Movie not found</h1>'
 				)
+})
+
+app.delete('/movies/:id', async function (req, res) {
+	const { id } = req.params
+	const result = await client
+		.db('classMongo')
+		.collection('movies')
+		.deleteOne({ id: id })
+	res.send(result)
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
