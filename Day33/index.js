de350c5e491f148332d09a1189657a2e6a88eee5
Maheshbabu Jeyaraj <j.maihby@gmail.com>
2022-04-28 15:42:03 +0530

    Done

--- Day33/index.js
+++ Day33/index.js
@@ -1,28 +1,39 @@
+// obvious requirement
 import express from 'express'
+// environment variables
 import dotenv from 'dotenv'
+// filesystem
 import * as fs from 'fs'
 
+// Create Express App
 const app = express()
 
+// Load local environment variables
 dotenv.config()
 
+// Read Port from .env file
 const PORT = process.env.PORT || 4001
 
+// Get unix Time stamp
 const ts_ms = +new Date()
+
+// Get time and date as a string
 const ts_str = new Date()
+
 // add 5.5 hours to ts_str
 ts_str.setHours(ts_str.getHours() + 5.5)
+
+// convert ts_str to ISO String for file name
 const date = ts_str.toISOString()
 
-// slice date upto 10th character
+// Getting only date and time from date string
 const date_str = date.slice(0, 10) + 'T' + date.slice(11, 19).replace(/:/g, ' ')
-
-console.log(date)
 
 app.get('/', (req, res) => {
 	res.send('Welcome to Day 33 Task')
 })
 
+// Create a file with the current time and date
 app.get('/writetime', (req, res) => {
 	fs.writeFile(
 		`./Timestamp/${date_str}.txt`,
@@ -39,6 +50,7 @@ app.get('/writetime', (req, res) => {
 	)
 })
 
+// Read all files 'txt' from the Timestamp directory
 app.get('/allfiles', (req, res) => {
 	const files = fs
 		.readdirSync('./Timestamp')
@@ -46,6 +58,7 @@ app.get('/allfiles', (req, res) => {
 	res.send(files)
 })
 
+// Start the server on the given port
 app.listen(PORT, () => {
-	console.log('Example app listening on port ' + PORT)
+	console.log('Day33 listening on port ' + PORT)
 })
